7366ee43a87e761d5fb99e1dbc8fe7df48e8a050
Erick Sánchez <ersajo@outlook.com>
2020-09-29 21:59:11 -0500

    Config eslint, build first endpoint and first configs

---
+++ src/utils/mongoClient.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const reducer = (acc, connection) => acc + `${connection.name} `;
+
+export default async () => {
+  try {
+    const { connections } = await mongoose.connect(
+      process.env.MONGO_URI,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+    );
+    const msg = connections.reduce(reducer, 'Connected to ');
+    console.log(msg);
+  } catch (err) {
+    console.log(err);
+  }
+};
