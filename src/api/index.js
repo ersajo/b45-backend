2f40e09968a792c64625531bbe65d84255b12806
Erick Sánchez <ersajo@outlook.com>
2020-10-02 21:19:17 -0500

    Validators and password hash

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,17 @@
 import express from 'express';
+import celebrate from 'celebrate'
 import router from '../routers/index.js';
+import middlewares from '../middlewares/index.js'
 
 export const app = express();
 export const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(express.json({ extended: true }))
+app.use(middlewares.getDate)
 
 app.get('/', (_, res) => res.send('Hello world'));
 // namespace
 app.use('/api/v1', router);
+
+app.use(celebrate.errors())
